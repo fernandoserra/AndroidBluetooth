b8e57eb6d9ce5e9555a1391eb632eb3ad0578411
Fernando Serra <fernando.serra.r@gmail.com>
2022-06-01 21:38:41 -0400

    Comprobando compatibilidad con dispositivo

--- app/src/main/java/com/example/androidbluetooth/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/androidbluetooth/ui/dashboard/DashboardFragment.kt
@@ -1,6 +1,8 @@
 package com.example.androidbluetooth.ui.dashboard
 
+import android.bluetooth.BluetoothAdapter
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,14 +10,23 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.androidbluetooth.databinding.FragmentDashboardBinding
+import javax.security.auth.login.LoginException
 
 class DashboardFragment : Fragment() {
+
+    private val TAG = "DashboardFragment"
 
     private var _binding: FragmentDashboardBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+
+    //**********
+    val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+    //**********
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,6 +43,15 @@ class DashboardFragment : Fragment() {
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+            Log.i(TAG, "El dispositivo no es compatible con bluetooth ")
+        }else{
+            Log.i(TAG, "Compatible ")
+        }
+
         return root
     }
 
