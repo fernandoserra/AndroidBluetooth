9007907f0713c9899943932aa1fece17e0a8a141
Fernando Serra <fernando.serra.r@gmail.com>
2022-06-01 21:28:58 -0400

    Actualizacion bluetooHeadSet

--- app/src/main/java/com/example/androidbluetooth/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/androidbluetooth/ui/home/HomeFragment.kt
@@ -1,6 +1,10 @@
 package com.example.androidbluetooth.ui.home
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothHeadset
+import android.bluetooth.BluetoothProfile
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,11 +15,40 @@ import com.example.androidbluetooth.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private val TAG = "HomeFragment"
+
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    ///////*****
+    //Por ejemplo, en este fragmento de código, se muestra la manera de conectarse a un objeto del proxy de BluetoothHeadset,
+    // de modo que se pueda controlar el perfil de auriculares:
+    var bluetoothHeadset: BluetoothHeadset? = null
+
+    // Get the default adapter
+    val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+
+    private val profileListener = object : BluetoothProfile.ServiceListener {
+
+        override fun onServiceConnected(profile: Int, proxy: BluetoothProfile) {
+            if (profile == BluetoothProfile.HEADSET) {
+                bluetoothHeadset = proxy as BluetoothHeadset
+                Log.i(TAG, "onServiceConnected: ${bluetoothHeadset.toString()}")
+            }
+        }
+
+        override fun onServiceDisconnected(profile: Int) {
+            if (profile == BluetoothProfile.HEADSET) {
+                bluetoothHeadset = null
+                Log.i(TAG, "onServiceDisconnected: $bluetoothHeadset ")
+            }
+        }
+    }
+    ///////*****
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,6 +65,10 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        // Establish connection to the proxy.
+        bluetoothAdapter?.getProfileProxy(context, profileListener, BluetoothProfile.HEADSET)
+
         return root
     }
 
