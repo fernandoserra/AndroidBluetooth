1677035b208633787474612e6ae1db89b1ac7b70
Fernando Serra <fernando.serra.r@gmail.com>
2022-06-03 15:36:57 -0400

    Actualizacion NotificationsFragmento

--- app/src/main/java/com/example/androidbluetooth/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/androidbluetooth/ui/notifications/NotificationsFragment.kt
@@ -1,6 +1,12 @@
 package com.example.androidbluetooth.ui.notifications
 
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,17 +17,14 @@ import com.example.androidbluetooth.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
 
+    private val TAG ="NotificationsFragment"
     private var _binding: FragmentNotificationsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?  ): View {
         val notificationsViewModel =
             ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
@@ -32,8 +35,49 @@ class NotificationsFragment : Fragment() {
         notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
         return root
     }
+
+    override fun onResume() {
+        super.onResume()
+
+        // Register for broadcasts when a device is discovered.
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        //context?.registerReceiver(receiver, filter)
+        //activity?.registerReceiver(receiver,filter)
+
+        context?.registerReceiver(receiver,filter)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        context?.unregisterReceiver(receiver)
+    }
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+
+            Log.i(TAG, "onReceive: Ejecutando BroadcastReceiver")
+            val action: String? = intent.action
+            when(action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    // Discovery has found a device. Get the BluetoothDevice
+                    // object and its info from the Intent.
+                    val device: BluetoothDevice? =
+                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                    Log.i(TAG, "onReceive: $device")
+                   // val deviceName = device.name
+                   // val deviceHardwareAddress = device.address // MAC address
+                }
+            }
+        }
+    }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
